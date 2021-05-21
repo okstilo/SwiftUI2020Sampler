6fe5fa6ab50fec2957131772c9e8629be3d2951f
Masaki Kitagawa <kitagawa.stilo@gmail.com>
2021-05-21 22:54:32 +0900

    create project

---
+++ SwiftUI2020Sampler/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI2020Sampler
+//
+//  Created by Masaki Kitagawa on 2021/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("ハローワールド")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI2020Sampler/SwiftUI2020SamplerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI2020SamplerApp.swift
+//  SwiftUI2020Sampler
+//
+//  Created by Masaki Kitagawa on 2021/05/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI2020SamplerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
