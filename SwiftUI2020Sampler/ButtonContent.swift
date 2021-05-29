864efd37f74e9ee2340a4b02d24bf8aa591401c8
Masaki Kitagawa <kitagawa.stilo@gmail.com>
2021-05-29 21:00:00 +0900

    Add ButtonContent

---
+++ SwiftUI2020Sampler/ButtonContent.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonContent.swift
+//  SwiftUI2020Sampler
+//
+//  Created by Masaki Kitagawa on 2021/05/30.
+//
+
+import SwiftUI
+
+struct ButtonContent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ButtonContent_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonContent()
+    }
+}
