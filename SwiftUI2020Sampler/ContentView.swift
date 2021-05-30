630fdab6d1dce2b2e0a50132b088b14598cfd02e
Masaki Kitagawa <kitagawa.stilo@gmail.com>
2021-05-30 18:05:40 +0900

    Change title

--- SwiftUI2020Sampler/ContentView.swift
+++ SwiftUI2020Sampler/ContentView.swift
@@ -44,7 +44,7 @@ struct ContentView: View {
                     }
                 }
             }
-            .navigationTitle("しりとり")
+            .navigationTitle("SwiftUI 2020 Sampler")
         }
     }
 }
