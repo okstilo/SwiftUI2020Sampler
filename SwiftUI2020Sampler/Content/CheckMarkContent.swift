63661c258846058c13050368b4b69f4b1910f14b
Masaki Kitagawa <kitagawa.stilo@gmail.com>
2021-06-19 16:54:23 +0900

    Add Folder, Clean file

---
+++ SwiftUI2020Sampler/Content/CheckMarkContent.swift
@@ -0,0 +1,20 @@
+//
+//  CheckMarkContent.swift
+//  SwiftUI2020Sampler
+//
+//  Created by Masaki Kitagawa on 2021/06/19.
+//
+
+import SwiftUI
+
+struct CheckMarkContent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CheckMarkContent_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckMarkContent()
+    }
+}
